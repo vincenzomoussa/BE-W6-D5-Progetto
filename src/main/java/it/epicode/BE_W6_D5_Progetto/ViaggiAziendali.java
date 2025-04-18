766ec742e902b97564f28c18a4bc53137a000e72
Vincenzo Moussa <vincenzomoussa@gmail.com>
2025-04-18 11:09:44 +0200

    Base creata

---
+++ src/main/java/it/epicode/BE_W6_D5_Progetto/ViaggiAziendali.java
@@ -0,0 +1,13 @@
+package it.epicode.BE_W6_D5_Progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaggiAziendali {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViaggiAziendali.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/BE_W6_D5_Progetto/ViaggiAziendaliTests.java
@@ -0,0 +1,13 @@
+package it.epicode.BE_W6_D5_Progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaggiAziendaliTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
